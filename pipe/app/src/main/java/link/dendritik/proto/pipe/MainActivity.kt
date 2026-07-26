7aadb003732375aaaca45ccf9f5f3ef2014c3b5e
Anugerah Christian Rompis <starleaf1@gmail.com>
2026-07-27 07:40:59 +0800

    feat: Initialize ProtoPipe project with basic structure and theming
    - Added Color.kt for color definitions used in the app theme.
    - Created Theme.kt to define light and dark color schemes and manage dynamic theming.
    - Introduced Type.kt for typography styles in the app.
    - Added keep rules for R8 configuration.
    - Created vector drawables for launcher icons (background and foreground).
    - Defined adaptive icons for different screen densities.
    - Added XML resources for colors, strings, and themes.
    - Implemented backup and data extraction rules XML files.
    - Created a sample unit test for initial testing.
    - Set up Gradle build files and properties for project configuration.
    - Established dependency management with versions for libraries and plugins.

---
+++ pipe/app/src/main/java/link/dendritik/proto/pipe/MainActivity.kt
@@ -0,0 +1,47 @@
+package link.dendritik.proto.pipe
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import link.dendritik.proto.pipe.ui.theme.ProtoPipeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ProtoPipeTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    ProtoPipeTheme {
+        Greeting("Android")
+    }
+}
